dc23bad375302c22bb7278ddda0869bf3afaa528
matt.matheus <matt.matheus@healthcatalyst.com>
2022-12-01 09:46:35 -0800

    clear extraneous spaces from solution file

--- Day1.ps1
+++ Day1.ps1
@@ -2,7 +2,6 @@ $Datum = get-Content .\Inputfiles\day1input.txt
 $elfPosition = 1
 $totalLoads = @()
 
-
 foreach($data in $datum){
     if([int]$data -ne 0){
         $elfLoad += [int]$data
@@ -20,7 +19,6 @@ foreach($data in $datum){
 
 $highestElf = $totalLoads | Sort-Object -Property ElfLoad
 $topElves = ($highestElf[-1].ElfLoad) + ($highestElf[-2].ElfLoad) + ($highestElf[-3].ElfLoad)
-
 
 Write-Output "Most Loaded Elf is at position $($highestElf[-1].ElfPosition) with load of $($highestElf[-1].ElfLoad)"
 Write-Output "The total for our top 3 elves is $($topElves)"
